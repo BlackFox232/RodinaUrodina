74dc7baa3a8d6490f7f886627db402ff33ef9e6b
Артем Симаков <artem.simakov.98@mail.ru>
2019-05-10 04:48:37 +0300

    bd

---
+++ RodinaUrodina/Requests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RodinaUrodina
+{
+    public partial class Requests : Form
+    {
+        public Requests()
+        {
+            InitializeComponent();
+        }
+
+        private void requestsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.requestsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.rodinaUrodinaDataSet);
+
+        }
+
+        private void Requests_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rodinaUrodinaDataSet.Requests". При необходимости она может быть перемещена или удалена.
+            this.requestsTableAdapter.Fill(this.rodinaUrodinaDataSet.Requests);
+
+        }
+    }
+}
