78a217c271467849efa4fa6a396bd50b359851e8
Артем Симаков <artem.simakov.98@mail.ru>
2019-05-11 03:57:11 +0300

    Design

--- RodinaUrodina/Forms/MyTours.Designer.cs
+++ RodinaUrodina/Forms/MyTours.Designer.cs
@@ -45,6 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(897, 450);
             this.Controls.Add(this.toursDataGridView);
             this.Name = "MyTours";
--- RodinaUrodina/Forms/Usezver_Form.Designer.cs
+++ RodinaUrodina/Forms/Usezver_Form.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Usezver_Form));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -37,9 +38,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(347, 124);
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(347, 99);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 26);
+            this.button1.Size = new System.Drawing.Size(192, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "Мои туры";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +50,11 @@
             // 
             // button2
             // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button2.Location = new System.Drawing.Point(347, 156);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(192, 28);
+            this.button2.Size = new System.Drawing.Size(192, 55);
             this.button2.TabIndex = 1;
             this.button2.Text = "Доступные туры";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,9 +62,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(347, 190);
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(347, 205);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(192, 25);
+            this.button3.Size = new System.Drawing.Size(192, 52);
             this.button3.TabIndex = 2;
             this.button3.Text = "Контакты";
             this.button3.UseVisualStyleBackColor = true;
@@ -67,9 +74,11 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(347, 221);
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(347, 263);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(192, 23);
+            this.button4.Size = new System.Drawing.Size(192, 56);
             this.button4.TabIndex = 3;
             this.button4.Text = "Связь с разработчиком";
             this.button4.UseVisualStyleBackColor = true;
@@ -77,9 +86,11 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(283, 250);
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(284, 325);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(308, 23);
+            this.button5.Size = new System.Drawing.Size(308, 66);
             this.button5.TabIndex = 4;
             this.button5.Text = "КНОПКА НАХОДИТСЯ В РАЗРАБОТКЕ!!!!!";
             this.button5.UseVisualStyleBackColor = true;
@@ -88,6 +99,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RodinaUrodina.Properties.Resources.depositphotos_63554371_stock_photo_beautiful_gray_background;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
